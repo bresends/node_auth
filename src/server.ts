b737399d582c1c63f208a75250e639fa66665dcc
bruno <bruno.resende@gmx.com>
2023-12-17 09:55:07 -0300

    Feat: Added Cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 import 'dotenv/config';
+import { rootRouter } from './routes/api/root.js';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
+const corsWhitelist = ['http://localhost:3000'];
+
+app.use(
+    cors({
+        origin: (origin = '', callback) => {
+            if (corsWhitelist.indexOf(origin) !== -1 || !origin) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        optionsSuccessStatus: 200,
+    })
+);
+
+app.use(express.json());
+app.use('/api', rootRouter);
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
