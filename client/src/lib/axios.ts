67969ebf3ebd575e6945f157b18e47ce84a99478
bruno <bruno.resende@gmx.com>
2023-12-27 16:09:27 -0300

    Fix: Cors for React

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,6 +1,15 @@
 import axiosInstance from 'axios';
 
+const API = 'http://localhost:3000';
+
 export const axios = axiosInstance.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: API,
     timeout: 15000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    withCredentials: true,
 });
+
+export const registerRequest = async (email: string, password: string) =>
+    axios.post('/api/register', { email, password });
