0f9bc187eae992f0daeeffa9b1c03282f4b89b44
bruno <bruno.resende@gmx.com>
2023-12-27 16:08:40 -0300

    Fix: Sending User Info on Server

--- server/src/routes/api/user.ts
+++ server/src/routes/api/user.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { db } from '../../database/prismaClient.js';
 import { verifyJWT } from '../../middleware/verifyJWT.js';
 import { verifyRole } from '../../middleware/verifyRoles.js';
 export const user = Router();
@@ -8,5 +9,20 @@ user.use(verifyJWT);
 user.use(verifyRole(['user', 'admin', 'editor']));
 
 user.get('/', async (req, res) => {
-    return res.status(200).json({ message: 'User route.' });
+    const userData = await db.user.findUnique({
+        where: {
+            id: req.userId,
+        },
+        select: {
+            name: true,
+            email: true,
+            roles: {
+                select: {
+                    name: true,
+                },
+            },
+        },
+    });
+
+    return res.status(200).json({ user: userData });
 });
