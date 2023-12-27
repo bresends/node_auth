02b498b37d0548a3521e75591007569eff2ed7ab
bruno <bruno.resende@gmx.com>
2023-12-27 16:09:18 -0300

    Feat: Added React Query

--- client/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Register } from './pages/Register';
-
-export default function Home() {
-    return (
-        <main className="flex justify-center items-center h-[100dvh]">
-            <Register />
-        </main>
-    );
-}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {
+    QueryClient,
+    QueryClientProvider
+} from '@tanstack/react-query';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import ErrorPage from './pages/404.tsx';
+import { Register } from './pages/Register.tsx';
+import { Root } from './pages/Root.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: '/register',
+        element: <Register />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: '/login',
+        element: <Root />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: '/tasks',
+        element: <Root />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: '/404',
+        element: <ErrorPage />,
+    },
+    {
+        path: '*',
+        element: <ErrorPage />,
+    },
+]);
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    </React.StrictMode>
+);
