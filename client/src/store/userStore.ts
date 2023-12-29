46bf647e9f09333e8ce7636a6de45bd656e0523c
bruno <bruno.resende@gmx.com>
2023-12-29 13:59:02 -0300

    Feat: Added User Role to Store

--- client/src/store/userStore.ts
+++ client/src/store/userStore.ts
@@ -2,10 +2,14 @@ import { create } from 'zustand';
 
 interface Session {
     token: string;
+    role: string;
     setToken: (time: string) => void;
+    setRole: (role: string) => void;
 }
 
 export const useAuthStore = create<Session>()((set) => ({
     token: '',
+    role: '',
     setToken: (token) => set({ token }),
+    setRole: (role) => set({ role }),
 }));
