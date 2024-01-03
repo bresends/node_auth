ad03f006104a1523d0973d0dcbb44ef2f9b1d06c
bruno <bruno.resende@gmx.com>
2024-01-03 17:55:16 -0300

    Feat: Added Index

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,18 @@
-import express, { Express } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import 'dotenv/config';
-import { rootRouter } from './routes/root';
-import { logger } from './lib/logger';
+import express, { Express, Request, Response } from 'express';
 import { pinoHttp } from 'pino-http';
+import { logger } from './lib/logger';
+import { rootRouter } from './routes/root';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS || '';
+const allowedOriginsArray = allowedOrigins
+    .split(',')
+    .map((item) => item.trim());
 
 app.use(
     pinoHttp({
@@ -13,10 +20,32 @@ app.use(
     })
 );
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Allow javascript to access the cookie in the browser
+app.use((req: Request, res: Response, next) => {
+    const origin = req.headers.origin;
+
+    if (allowedOriginsArray.includes(origin as string)) {
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+    }
+
+    next();
 });
 
+app.use(
+    cors({
+        origin: (origin = '', callback) => {
+            if (allowedOriginsArray.indexOf(origin) !== -1 || !origin) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        optionsSuccessStatus: 200,
+    })
+);
+
+app.use(express.json());
+app.use(cookieParser());
 app.use('/api', rootRouter);
 
 app.listen(port, () => {
