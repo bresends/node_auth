6ebd1c93963ddc8682126ef4c1b9d0d803f7ec91
bruno <bruno.resende@gmx.com>
2024-01-03 07:44:54 -0300

    Refactor: Drizzle Refresh Token

--- server/src/routes/api/auth/refresh.ts
+++ server/src/routes/api/auth/refresh.ts
@@ -1,53 +1,60 @@
 import { Request, Response, Router } from 'express';
 import jsonwebtoken from 'jsonwebtoken';
-import { db } from '@/database/prismaClient.js';
+import { db } from '@/database/drizzleClient.js';
+import { refreshToken, users } from '@/database/schema.js';
+import { eq } from 'drizzle-orm';
 
 const { sign, verify } = jsonwebtoken;
 
 export const refresh = Router();
 
 refresh.get('/', async (req: Request, res: Response) => {
-    const refreshToken = req.cookies.jwt as string;
+    const oldRefreshToken = req.cookies.jwt as string;
 
-    console.log('Tried Login with:', refreshToken);
-
-    if (!refreshToken) return res.sendStatus(401);
+    if (!oldRefreshToken) return res.sendStatus(401);
 
     try {
-        const user = await db.user.findFirst({
-            where: { refreshToken: { some: { token: refreshToken } } },
-        });
+        const user = await db
+            .select({
+                id: users.id,
+            })
+            .from(refreshToken)
+            .where(eq(refreshToken.token, oldRefreshToken))
+            .leftJoin(users, eq(refreshToken.userId, users.id));
 
-        // Step 1: Check if user if the previous refresh token exists
+        // Step 1: Check if user previous refresh token exists
         // Step 2: Check if the previous refresh token is valid (if it is not I can just block the attacker)
         // Step 3: If the previous refresh token is valid, but no user has it associated, delete all refresh tokens associated with the user ()
         if (!user) {
             verify(
-                refreshToken,
+                oldRefreshToken,
                 process.env.REFRESH_TOKEN_SECRET as string,
                 async (err, decoded) => {
                     if (err) return res.sendStatus(403); // Forbidden
                     // Malicious user trying to use a refresh token that was already deleted
                     // Delete all refresh tokens associated with the attempted user
-                    await db.refreshToken.deleteMany({
-                        where: {
-                            userId: (decoded as { userId: number }).userId,
-                        },
-                    });
+                    await db
+                        .delete(refreshToken)
+                        .where(
+                            eq(
+                                refreshToken.userId,
+                                (decoded as { userId: number }).userId
+                            )
+                        );
                 }
             );
-            return res.sendStatus(403);
+            return res.sendStatus(403); // Forbidden
         }
 
         verify(
-            refreshToken,
+            oldRefreshToken,
             process.env.REFRESH_TOKEN_SECRET as string,
             async (err, decoded) => {
                 if (err) {
                     if (err.name === 'TokenExpiredError') {
-                        await db.refreshToken.delete({
-                            where: { token: refreshToken },
-                        });
+                        await db
+                            .delete(refreshToken)
+                            .where(eq(refreshToken.token, oldRefreshToken));
                     }
                     return res.sendStatus(403);
                 }
@@ -56,31 +63,29 @@ refresh.get('/', async (req: Request, res: Response) => {
                     userId: number;
                 };
 
-                if (userId !== user.id) return res.sendStatus(403);
+                if (userId !== user[0]?.id) return res.sendStatus(403);
 
                 const newAccessToken = sign(
-                    { userId: user.id },
+                    { userId: user[0].id },
                     process.env.ACESS_TOKEN_SECRET as string,
                     { expiresIn: '30s' }
                 );
 
                 const newRefreshToken = sign(
-                    { userId: user.id },
+                    { userId: user[0].id },
                     process.env.REFRESH_TOKEN_SECRET as string,
                     { expiresIn: '1d' }
                 );
 
                 // Delete previous refresh token
-                await db.refreshToken.delete({
-                    where: { token: refreshToken },
-                });
+                await db
+                    .delete(refreshToken)
+                    .where(eq(refreshToken.token, oldRefreshToken));
 
                 // Store new refresh token in db
-                await db.user.update({
-                    where: { id: user.id },
-                    data: {
-                        refreshToken: { create: { token: newRefreshToken } },
-                    },
+                await db.insert(refreshToken).values({
+                    token: newRefreshToken,
+                    userId: user[0].id,
                 });
 
                 res.cookie('jwt', newRefreshToken, {
